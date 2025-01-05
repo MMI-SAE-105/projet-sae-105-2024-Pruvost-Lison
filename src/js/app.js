fd542c18a37906f27838a903e39a708df668e285
Pruvost-Lison <lison.pruvost@edu.univ-fcomte.fr>
2025-01-05 23:58:28 +0100

    test menu, css de la timeline, ajout d'éléments JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,26 @@
+// commande menu
+// Sélection des éléments
+const btnMenu = document.getElementById('btnmenu');
+const menuPanel = document.getElementById('panel');
+const btnClose = document.getElementById('hide-menu');
+
+// Ouvrir le menu
+btnMenu.addEventListener('click', () => {
+    menuPanel.classList.add('is-open');
+    menuPanel.setAttribute('aria-hidden', 'false');
+    btnMenu.setAttribute('aria-expanded', 'true');
+});
+
+// Fermer le menu
+btnClose.addEventListener('click', () => {
+    menuPanel.classList.remove('is-open');
+    menuPanel.setAttribute('aria-hidden', 'true');
+    btnMenu.setAttribute('aria-expanded', 'false');
+});
+
+
+
+
 // commande accordéon
 document.querySelectorAll('.accordion__header').forEach(header => {
     header.addEventListener('click', () => {
