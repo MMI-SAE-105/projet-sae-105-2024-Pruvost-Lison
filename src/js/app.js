903a7e70bcc75cc0c8ebcd924d7c62b29bd2e959
Pruvost-Lison <lison.pruvost@edu.univ-fcomte.fr>
2025-01-06 13:25:47 +0100

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,42 @@
-// commande menu
-// Sélection des éléments
-const btnMenu = document.getElementById('btnmenu');
-const menuPanel = document.getElementById('panel');
-const btnClose = document.getElementById('hide-menu');
+// //commande menu
+// // Sélection des éléments
+// const btnMenu = document.getElementById('btnmenu');
+// const menuPanel = document.getElementById('panel');
+// const btnClose = document.getElementById('hide-menu');
 
-// Ouvrir le menu
-btnMenu.addEventListener('click', () => {
-    menuPanel.classList.add('is-open');
-    menuPanel.setAttribute('aria-hidden', 'false');
-    btnMenu.setAttribute('aria-expanded', 'true');
+// // Ouvrir le menu
+// btnMenu.addEventListener('click', () => {
+//     menuPanel.classList.add('is-open');
+//     menuPanel.setAttribute('aria-hidden', 'false');
+//     btnMenu.setAttribute('aria-expanded', 'true');
+// });
+
+// // Fermer le menu
+// btnClose.addEventListener('click', () => {
+//     menuPanel.classList.remove('is-open');
+//     menuPanel.setAttribute('aria-hidden', 'true');
+//     btnMenu.setAttribute('aria-expanded', 'false');
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const panel = document.getElementById('panel');
+    const openMenuButton = document.getElementById('btnmenu');
+    const closeMenuButton = document.getElementById('hide-menu');
+
+    // Ouvrir le menu
+    openMenuButton.addEventListener('click', function () {
+        panel.classList.add('go-right');
+        panel.setAttribute('aria-hidden', 'false'); // Accessibilité
+    });
+
+    // Fermer le menu
+    closeMenuButton.addEventListener('click', function () {
+        panel.classList.remove('go-right');
+        panel.setAttribute('aria-hidden', 'true'); // Accessibilité
+    });
 });
 
-// Fermer le menu
-btnClose.addEventListener('click', () => {
-    menuPanel.classList.remove('is-open');
-    menuPanel.setAttribute('aria-hidden', 'true');
-    btnMenu.setAttribute('aria-expanded', 'false');
-});
+
 
 
 
