f41e3c2d1415fe4d7a1c754b0c03e3cd75473307
Pruvost-Lison <lison.pruvost@edu.univ-fcomte.fr>
2025-01-06 16:33:17 +0100

    test caroussel

--- src/js/app.js
+++ src/js/app.js
@@ -18,10 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
-
-
-
 // commande accordéon
 document.querySelectorAll('.accordion__header').forEach(header => {
     header.addEventListener('click', () => {
@@ -30,3 +26,30 @@ document.querySelectorAll('.accordion__header').forEach(header => {
         header.classList.toggle('active');
     });
 });
+
+// commande caroussel
+
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const wrapper = document.querySelector('.wrapper');
+let currentIndex = 0;
+
+const totalItems = document.querySelectorAll('.wrapper .element').length;
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? totalItems - 1 : currentIndex - 1;
+    updateCaroussel();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === totalItems - 1) ? 0 : currentIndex + 1;
+    updateCaroussel();
+});
+
+function updateCaroussel() {
+    const offset = -currentIndex * 100; // Calcule l'offset pour faire défiler les images
+    wrapper.style.transform = `translateX(${offset}%)`; // Applique l'offset à la wrapper
+}
+
+
+
